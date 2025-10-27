a6e3dd99746e0bfc9367077bcbf3b1e9562029e6
chinmay <b22bb016@iitj.ac.in>
2025-10-28 01:17:41 +0530

    minor changes

--- job_run.sh
+++ job_run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=test_qwen
+#SBATCH --job-name=3_train_annotation
 #SBATCH --output=/scratch/data/bikash_rs/vivek/MER-Factory/logs/%x_%j.out
 #SBATCH --error=/scratch/data/bikash_rs/vivek/MER-Factory/logs/%x_%j.err
-#SBATCH --partition=dgx
+#SBATCH --partition=fat
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=64G
-#SBATCH --time=02:00:00
+#SBATCH --mem=48G
+#SBATCH --time=5-00:00:00
 #SBATCH --qos=fatqos
 #SBATCH -D /scratch/data/bikash_rs/vivek/MER-Factory
 
@@ -29,4 +29,5 @@ export HF_DATASETS_CACHE=$HF_HOME
 mkdir -p $HF_HOME
 
 # Run test script
-python test.py
+# python test.py
+python main.py /scratch/data/bikash_rs/vivek/dataset/MELD.Raw/train_splits_3 /scratch/data/bikash_rs/vivek/dataset/Meld_feat_ext/train_annotation --type mer --huggingface-model "Qwen/Qwen2.5-Omni-3B" --cache --threshold 0.8 --peak_dis 15 --concurrency 1
